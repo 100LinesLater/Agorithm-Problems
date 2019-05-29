e6267e86219803cf070234fbe158492e4ea972c6
Yoni Hartmayer <yonihart416@gmail.com>
2019-05-28 18:05:41 -0700

    Add palindrome number problem

---
+++ palindrome_number.js
@@ -0,0 +1,11 @@
+const isPalindrome = function (x) {
+  let str = x.toString();
+  let i = 0;
+  let j = str.length - 1;
+  while (i <= j) {
+    if (str[i++] !== str[j--]) {
+      return false;
+    }
+  }
+  return true;
+};
